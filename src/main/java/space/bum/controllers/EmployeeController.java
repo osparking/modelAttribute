b3c59c9c6426acbec5083f3972b6799282a8bb8f
osparking <jbpark03@gmail.com>
2024-03-20 18:22:04 +0900

    직원정보 수령 및 처리기능의  개선된 처리기

--- src/main/java/space/bum/controllers/EmployeeController.java
+++ src/main/java/space/bum/controllers/EmployeeController.java
@@ -10,14 +10,18 @@ import space.bum.model.Employee;
 
 @Controller
 public class EmployeeController {
-	@PostMapping("/addEmployee")
-	public String submit(@ModelAttribute("employee") Employee employee) {
-		System.out.println(employee);
-		return "employeeView";
-	}
   @GetMapping("/employeeInfo")
   public String getEmployeeInfoForm() {
     return "employeeInfo";
   }
 
+  @PostMapping("/addEmployee")
+  public String submit(@ModelAttribute("employee") Employee employee,
+      Model model) {
+    System.out.println(employee);
+    model.addAttribute("id", "0" + employee.getId());
+    model.addAttribute("name", employee.getName() + "씨");
+    model.addAttribute("empInfo", employee.toString());
+    return "employeeInfo";
+  }
 }
