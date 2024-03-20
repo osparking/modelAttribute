a2a1843e48435242c7bbe649e8536ceee229623d
osparking <jbpark03@gmail.com>
2024-03-20 18:13:57 +0900

    직원 정보 삽입 뷰 요청 처리기

--- src/main/java/space/bum/controllers/EmployeeController.java
+++ src/main/java/space/bum/controllers/EmployeeController.java
@@ -1,6 +1,8 @@
 package space.bum.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,4 +15,9 @@ public class EmployeeController {
 		System.out.println(employee);
 		return "employeeView";
 	}
+  @GetMapping("/employeeInfo")
+  public String getEmployeeInfoForm() {
+    return "employeeInfo";
+  }
+
 }
